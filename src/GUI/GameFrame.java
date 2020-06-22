02a2dbc520002910bd89cac6bdb214077c6a499d
9726081 <9726081@ceit.local>
2020-06-22 10:46:34 +0430

    "Create Game base Frame"

---
+++ src/GUI/GameFrame.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import javax.swing.*;
+
+/**
+ * base frame of game
+ *
+ * @author Amir Naziri
+ * @author faraz farangizade
+ */
+public class GameFrame extends JFrame
+{
+    /**
+     * create a new Game Frame
+     */
+    public GameFrame ()
+    {
+        super();
+
+        // settings here
+    }
+
+}
