a91b36780b4ebd97fb783e922c7dc216e6934872
9726060 <9726060@ceit.local>
2020-08-05 20:22:39 +0430

    Add javaDoc

--- src/MultiGame/Status/GameStatus.java
+++ src/MultiGame/Status/GameStatus.java
@@ -5,6 +5,10 @@ import MultiGame.Game.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * this classs saves the data of the multiPlayer game
+ */
+
 public class GameStatus implements Serializable
 {
 
@@ -19,6 +23,14 @@ public class GameStatus implements Serializable
     private boolean newPrize , usePrize , shot , explode;
     private ArrayList<String> names;
 
+    /**
+     *
+     * @param tanks tanks
+     * @param bullets bullets
+     * @param maps maps
+     * @param prizes prizes
+     * @param players number of players
+     */
     public GameStatus(ArrayList<TankMulti> tanks, InteractArrayListMulti<BulletMulti> bullets ,
                       MapsMulti maps, PrizesMulti prizes,int players)
     {
@@ -37,6 +49,16 @@ public class GameStatus implements Serializable
         this.players = players;
     }
 
+    /**
+     * updates the status of the game
+     * @param tanks tanks
+     * @param bullets bullets
+     * @param maps maps
+     * @param prizes prizes
+     * @param players players
+     * @param kills kills
+     * @param names names
+     */
     public void update(ArrayList<TankMulti> tanks, InteractArrayListMulti<BulletMulti> bullets ,
                        MapsMulti maps, PrizesMulti prizes,int players,int[] kills,ArrayList<String> names)
     {
@@ -49,101 +71,185 @@ public class GameStatus implements Serializable
         this.players = players;
     }
 
+    /**
+     *
+     * @return isGameOverAll
+     */
     public boolean isGameOverAll()
     {
         return gameOverAll;
     }
 
+    /**
+     * setGameOverAll
+     * @param gameOverAll
+     */
     public void setGameOverAll(boolean gameOverAll)
     {
         this.gameOverAll = gameOverAll;
     }
 
+    /**
+     *
+     * @return tanks field
+     */
     public ArrayList<TankMulti> getTanks() {
         return tanks;
     }
 
+    /**
+     *
+     * @return bullets field
+     */
     public InteractArrayListMulti<BulletMulti> getBullets() {
         return bullets;
     }
 
+    /**
+     *
+     * @return map filed
+     */
     public MapsMulti getMaps() {
         return maps;
     }
 
+    /**
+     *
+     * @return prizes field
+     */
     public PrizesMulti getPrizes() {
         return prizes;
     }
 
+    /**
+     *
+     * @return players field
+     */
     public int getPlayers() {
         return players;
     }
 
+    /**
+     *
+     * @param newPrize set new Prize
+     */
     public void setNewPrize(boolean newPrize)
     {
         this.newPrize = newPrize;
     }
 
+    /**
+     *
+     * @return new prize
+     */
     public boolean isNewPrize()
     {
         return newPrize;
     }
 
+    /**
+     *
+     * @param usePrize set use prize
+     */
     public void setUsePrize(boolean usePrize)
     {
         this.usePrize = usePrize;
     }
 
+    /**
+     *
+     * @return usePrize
+     */
     public boolean isUsePrize()
     {
         return usePrize;
     }
 
+    /**
+     * set shot
+     * @param shot
+     */
     public void setShot(boolean shot)
     {
         this.shot = shot;
     }
 
+    /**
+     *
+     * @return shot field
+     */
     public boolean isShot()
     {
         return shot;
     }
 
+    /**
+     * e explode field
+     * @param explode
+     */
     public void setExplode(boolean explode)
     {
         this.explode = explode;
     }
 
+    /**
+     *
+     * @return explode
+     */
     public boolean isExplode()
     {
         return explode;
     }
 
+    /**
+     *
+     * @return gameOVer
+     */
     public boolean isGameOver()
     {
         return gameOver;
     }
 
+    /**
+     * set game over
+     * @param gameOver
+     */
     public void setGameOver(boolean gameOver)
     {
         this.gameOver = gameOver;
     }
 
+    /**
+     *
+     * @return winners field
+     */
     public ArrayList<String> getWinners()
     {
         return winners;
     }
 
+    /**
+     *
+     * @param temp
+     */
     public void addWinners(String temp)
     {
         winners.add(temp);
     }
 
+    /**
+     *
+     * @return kills
+     */
     public int[] getKills()
     {
         return kills;
     }
 
+    /**
+     *
+     * @return names
+     */
     public ArrayList<String> getNames()
     {
         return names;
